6486bbf1f67f4dc72cd72aeccd57ad615babdc60
Saeed Kefayati <55033069+saeedkefayati@users.noreply.github.com>
2025-10-02 15:08:01 +0330

    update draw_progress_bar function

--- utils/main.sh
+++ utils/main.sh
@@ -13,22 +13,32 @@ draw_banner() {
 
 # Progress Bar
 draw_progress_bar() {
-    local label=$1 used=$2 total=$3 used_hr=$4 total_hr=$5
+    local label=$1
+    local used=$2
+    local total=$3
+    local used_hr=$4
+    local total_hr=$5
+
     local percentage=0
     [ "$total" -ne 0 ] && percentage=$(( (100 * used) / total ))
     [ "$percentage" -gt 100 ] && percentage=100
 
     local color reset="\033[0m"
-    if   [ "$percentage" -lt 70 ]; then color="\033[0;32m"
-    elif [ "$percentage" -lt 90 ]; then color="\033[1;33m"
-    else color="\033[0;31m"; fi
+    if [ "$percentage" -lt 70 ]; then
+        color="\033[0;32m"
+    elif [ "$percentage" -lt 90 ]; then
+        color="\033[1;33m"
+    else
+        color="\033[0;31m"
+    fi
 
     local filled=$(( (percentage * 20) / 100 ))
     local empty=$(( 20 - filled ))
 
-    local bar
-    bar="$(printf "%${filled}s" | tr ' ' '█')"
-    bar+=$(printf "%${empty}s" | tr ' ' '░')
+    local bar_filled bar_empty
+    bar_filled=$(printf "%${filled}s" | tr ' ' '#')
+    bar_empty=$(printf "%${empty}s" | tr ' ' '-')
+    local bar="${bar_filled}${bar_empty}"
 
     printf "%-12s [${color}%s${reset}] %3d%% (%s / %s)\n" \
         "$label" "$bar" "$percentage" "$used_hr" "$total_hr"
